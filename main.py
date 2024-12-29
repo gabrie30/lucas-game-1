8b8f0917327b9ac48aa1341da2a48edf03c7d953
gabrie30 <jgabriels30@gmail.com>
2024-12-29 13:56:54 -0800

    Added title, made previous guesses show if the guess was to high or low, changed the way the previous guesses look.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import random
-import sys
-import os
-def clear_screen():
-    sys.stdout.flush()
-    os.system('cls' if os.name == 'nt' else 'clear')
+from define import clear_screen, you_win, game_over, title_art
+
 clear_screen()
-num_high = int(input("choose between 0 and #: "))
+
+title_art()
+print("By Lucas Jantz")
+print("Play in full screen if the askii art is the small")
+
+
+
+num_high = int(input("choose a maximum number : "))
 random_number = random.randint(0, num_high)
 guess = num_high + 200
-difficulty = str(input("choose difficulty, easy = 25 attempts, medium = 10 attempts, hard = 5 attempt, custom = custom atempts:    "))
+difficulty = str(input("choose difficulty, easy = 25 attempts, medium = 10 attempts, hard = 5 attempt, custom = custom atempts: "))
 if difficulty == "easy":
         attempts = 25
 elif difficulty == "medium":
@@ -16,20 +20,11 @@ elif difficulty == "medium":
 elif difficulty == "hard":
         attempts = 5
 elif difficulty == "custom":
-        attempts = int(input("choose attempt amounts"))
+        attempts = int(input("choose attempt amounts "))
 
 print(f"Find the number between 0 and {num_high} to win.")
 
-clear_screen
-
-def you_win():
-    print("Congratulations! \n"
-              " ____    ____  ______    __    __     ____    __    ____  __  .__   __.\n"
-              "\\   \\  /   / /  __  \\  |  |  |  |    \\   \\  /  \\  /   / |  | |  \\ |  |\n"
-              " \\   \\/   / |  |  |  | |  |  |  |     \\   \\/    \\/   /  |  | |   \\|  |\n"
-              "  \\_    _/  |  |  |  | |  |  |  |      \\            /   |  | |  . `  |\n"
-              "    |  |    |  `--'  | |  `--'  |       \\    /\\    /    |  | |  |\\   |\n"
-              "    |__|     \\______/   \\______/         \\__/  \\__/     |__| |__| \\__|")
+clear_screen()
 
 
 
@@ -38,20 +33,26 @@ prev_guesses = []
 
 while random_number != guess:
     attempts = attempts - 1
-    guess = int(input("Please guess a number:"))
+    guess = int(input(f"Please guess a number between 0 and {num_high}: "))
     clear_screen()
-    prev_guesses.append(guess)
-    print(f"You have {attempts} attempts left.")
-    print(f"Previous guesses: {prev_guesses}")
     if attempts < 1:
-        print("You've run out of attempts! Game over.")
+        game_over()
         break
     if guess == random_number:
         you_win()
     elif guess > random_number:
         print("guess is too big!")
+        prev_guesses.append(f"{guess} is to high")
     elif guess < random_number:
         print("guess is to small!")
+        prev_guesses.append(f"{guess} is to low")
+    print(f"You have {attempts} attempts left.")
+    print(f"Previous guesses:")
+    for i in prev_guesses:
+        print(f"  * {i}")
+
+
+
 
 
 
